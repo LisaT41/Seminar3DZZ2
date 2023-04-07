69ae3b01d18f4bb3dc122da3ab5cdb84fbdcf09b
l.gor41000@yandex.ru <l.gor41000@yandex.ru>
2023-04-07 23:02:34 +0200

    Create seconde task Seminar3

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+int ReadLInt(string message)
+{
+    Console.Write($"{message} > ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+int ax = ReadLInt("Koord x first point");
+int ay = ReadLInt("Koord y first point");
+int bx = ReadLInt("Koord x seconde point");
+int by = ReadLInt("Koord y seconde point");
+int cx = ReadLInt("Koord x third point");
+int cy = ReadLInt("Koord y third point");
+
+int koordX = (ax - bx - cx);
+int koordY = (ay - by - cx);
+
+double W = Math.Sqrt(Math.Pow(koordX,2) + Math.Pow(koordY,2));
+Console.WriteLine(W);
+Console.WriteLine($"Shox result {W:f2}" );
